2ead269b169a2a0fb2547bc3e437a0e5cc445128
JagathA <jagath.ariy@gmail.com>
2023-02-12 01:21:00 +0000

    initial Version

---
+++ src/components/PlanetName.test.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import PlanetName, { PlanetNameProps } from './PlanetName';
+import user from '@testing-library/user-event';
+
+describe("<Planetname>", () => {
+	const labelText = "Planet Name";
+
+	test(`Given the required props, 
+		When the component is rendered
+		Then the label text "Planet Name" should be present`, () => {
+			const mockChange = jest.fn();
+			const requiredProps: PlanetNameProps = {
+				planetName: "Earth",
+				onChange: mockChange,
+			}
+			render(<PlanetName {...requiredProps} />);
+			expect(screen.getByText(labelText)).toBeInTheDocument();
+		});
+
+		test(`Given the required props, 
+		When the component is rendered
+		Then the vaues passed in props should be present`, () => {
+			const mockChange = jest.fn();
+			const PlanetNameText = "Earth";
+			const requiredProps: PlanetNameProps = {
+				planetName: "Earth",
+				onChange: mockChange,
+			}
+			render(<PlanetName {...requiredProps} />);
+			expect(screen.getByDisplayValue(PlanetNameText)).toBeInTheDocument();
+		});
+
+		test(`Given component is rendered, 
+		when the user inputs text,
+		Then onChange function is called the corrcet number of times`, async() => {
+			const mockChange = jest.fn();
+			const requiredProps: PlanetNameProps = {
+				planetName: "Earth",
+				onChange: mockChange,
+			}
+			render(<PlanetName {...requiredProps} />);
+			await user.type(screen.getByLabelText(labelText),"Jupiter");
+        expect(requiredProps.onChange).toHaveBeenCalledTimes(7);
+		});
+	});
