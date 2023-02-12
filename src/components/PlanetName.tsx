7a00a394164059ad77c6853192ca0963c193d03e
JagathA <jagath.ariy@gmail.com>
2023-02-12 01:20:29 +0000

    Export Interface

--- src/components/PlanetName.tsx
+++ src/components/PlanetName.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
 
-interface PlanetNameProps {
+
+export interface PlanetNameProps {
 	planetName: string;
-	onChange: (newValue:string) => void;
+	onChange: (newValue: string) => void;
 }
 
-	const PlanetName: React.FC<PlanetNameProps> = ({ planetName, onChange }) => (
-		<>
-			<label htmlFor='planetName'>Planet Name</label>
-			<input id='planetName' type='text' value={planetName} onChange={(e)=>onChange(e.target.value)} />
-		</>);
+const PlanetName: React.FC<PlanetNameProps> = ({ planetName, onChange }) => (
+	<>
+		<label htmlFor='planetName'>Planet Name</label>
+		<input id='planetName' type='text' value={planetName} onChange={(e) => onChange(e.target.value)} />
+	</>);
 
-	export default PlanetName;
+export default PlanetName;
