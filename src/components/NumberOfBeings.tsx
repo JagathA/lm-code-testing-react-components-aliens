97fdd875c93e5fdb6180cf3e07652331593b4462
JagathA <jagath.ariy@gmail.com>
2023-02-12 15:18:00 +0000

    NumberOfBeings changed to number type

--- src/components/NumberOfBeings.tsx
+++ src/components/NumberOfBeings.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-interface NumberOfBeingsProps {
-	numberOfBeings: string;
-	onChange: (newValue:string) => void;
+export interface NumberOfBeingsProps {
+	numberOfBeings: number;
+	onChange: (newValue:number) => void;
 }
 
 	const NumberOfBeings: React.FC<NumberOfBeingsProps> = ({ numberOfBeings, onChange }) => (
 		<>
 			<label htmlFor='NumberOfBeings'>Number of Beings</label>
-			<input id='NumberOfBeings' type='text' value={numberOfBeings} onChange={(e)=>onChange(e.target.value)} />
+			<input id='NumberOfBeings' type='number' value={numberOfBeings} onChange={(e)=>onChange(e.target.valueAsNumber)} />
 		</>);
 
 	export default NumberOfBeings;
