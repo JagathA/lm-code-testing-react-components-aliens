a6d4c0973b6f1182f88e9d83fc09ed35106f8efc
JagathA <jagath.ariy@gmail.com>
2023-02-19 16:20:43 +0000

    added validation and error message

--- src/components/SpeciesName.tsx
+++ src/components/SpeciesName.tsx
@@ -1,14 +1,44 @@
+import { constants } from 'http2';
 import React from 'react';
+import { useState } from 'react';
+import ErrorMessage from './ErrorMessage';
 
 export interface SpeciesNameProps {
 	speciesName: string;
-	onChange: (newValue:string) => void;
+	onChange: (newValue: string) => void;
 }
 
-	const SpeciesName: React.FC<SpeciesNameProps> = ({ speciesName, onChange }) => (
+const SpeciesName: React.FC<SpeciesNameProps> = ({ speciesName, onChange }) => {
+
+	const [errorMessage, setErrorMessage] =
+		useState<string | undefined>(undefined);
+
+	const validate: (value: string) => string | undefined = (value) => {
+		const reg = /^[a-z]+$/i;
+		const errLbl = "Error:"
+		if (!reg.test(value)) {
+			return `${errLbl} No numbers or special characters allowed!`
+		}
+
+		else if (value.length < 3 || value.length > 23) {
+			return `${errLbl} Must be between 3 and 23 characters`
+		}
+
+		return undefined;
+	}
+	return (
 		<>
 			<label htmlFor='speciesName'>Species Name</label>
-			<input id='speciesName' type='text' value={speciesName} onChange={(e)=>onChange(e.target.value)} />
-		</>);
+			<input id='speciesName' type='text' value={speciesName} onChange={
+				(e) => {
+					const errorMessage = validate(e.target.value);
+					setErrorMessage(errorMessage);
+					onChange(e.target.value)
+				}}
+			/>
+			<ErrorMessage errorMessage={errorMessage} />
+		</>
+	)
+}
 
-	export default SpeciesName;
+export default SpeciesName;
