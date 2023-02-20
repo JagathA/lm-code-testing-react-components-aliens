30c25342b09016990fa133c267702974d103e2fc
JagathA <jagath.ariy@gmail.com>
2023-02-20 01:10:43 +0000

    Added validation tests

--- src/components/SpeciesName.tsx
+++ src/components/SpeciesName.tsx
@@ -1,4 +1,3 @@
-import { constants } from 'http2';
 import React from 'react';
 import { useState } from 'react';
 import ErrorMessage from './ErrorMessage';
@@ -8,20 +7,31 @@ export interface SpeciesNameProps {
 	onChange: (newValue: string) => void;
 }
 
+const errLbl = "ERROR:"
+const SPECIES_NAME_MIN_LENGTH = 3;
+const SPECIES_NAME_MAX_LENGTH = 23;
+
+export const errorSpeciesNameTooShort = `${errLbl} Species Name must be minimum ${SPECIES_NAME_MIN_LENGTH} characters`
+export const errorSpeciesNameTooLong = `${errLbl} Species Name must be maximum ${SPECIES_NAME_MAX_LENGTH} characters`
+export const errorSpeciesNameInvalidCharacters = `${errLbl} No numbers or special characters allowed!`
+
 const SpeciesName: React.FC<SpeciesNameProps> = ({ speciesName, onChange }) => {
 
 	const [errorMessage, setErrorMessage] =
 		useState<string | undefined>(undefined);
 
 	const validate: (value: string) => string | undefined = (value) => {
-		const reg = /^[a-z]+$/i;
-		const errLbl = "Error:"
-		if (!reg.test(value)) {
-			return `${errLbl} No numbers or special characters allowed!`
-		}
-
-		else if (value.length < 3 || value.length > 23) {
-			return `${errLbl} Must be between 3 and 23 characters`
+		const reg = /^[a-z]+$/i;;
+		if (value.length > 0) {
+			if (!reg.test(value)) {
+				return errorSpeciesNameInvalidCharacters;
+			}
+			else if (value.length < SPECIES_NAME_MIN_LENGTH) {
+				return errorSpeciesNameTooShort;
+			}
+			else if (value.length > SPECIES_NAME_MAX_LENGTH) {
+				return errorSpeciesNameTooLong;
+			}
 		}
 
 		return undefined;
