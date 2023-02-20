0d10efae4becd3ac74db1c09178ed0b8a8432949
JagathA <jagath.ariy@gmail.com>
2023-02-20 01:10:25 +0000

    added validation and error message element

--- src/components/SpeciesName.test.tsx
+++ src/components/SpeciesName.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import SpeciesName, { SpeciesNameProps } from './SpeciesName';
+import SpeciesName, { SpeciesNameProps, errorSpeciesNameTooShort, errorSpeciesNameTooLong, errorSpeciesNameInvalidCharacters } from './SpeciesName';
 import user from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 
 describe("<SpeciesName>", () => {
 	const labelText = "Species Name";
@@ -41,4 +42,65 @@ describe("<SpeciesName>", () => {
 			await user.type(screen.getByLabelText(labelText),"Venusians");
         expect(requiredProps.onChange).toHaveBeenCalledTimes(9);
 		});
+
+		test(`Given use inputs a valid inputs for Species name (between 3 and 23 characters ), 
+		when the component is rendered,
+		An error message is not displayed`, async() => {
+			const mockChange = jest.fn();
+			const requiredProps: SpeciesNameProps = {
+				speciesName: "morethantwentythreecha",
+				onChange: mockChange,
+			}
+			render(<SpeciesName {...requiredProps} />);
+			await user.type(screen.getByLabelText(labelText),"m");
+			expect(screen.queryByText(errorSpeciesNameTooLong)).not.toBeInTheDocument();
+			expect(screen.queryByText(errorSpeciesNameTooShort)).not.toBeInTheDocument();
+			expect(screen.queryByText(errorSpeciesNameInvalidCharacters)).not.toBeInTheDocument();
+
+			
+		});
+
+		test(`Given use inputs an invalid inputs for Species name (less than 3 characters), 
+		when the component is rendered,
+		The correct error message is displayed`, async() => {
+			const mockChange = jest.fn();
+			const requiredProps: SpeciesNameProps = {
+				speciesName: "V",
+				onChange: mockChange,
+			}
+			render(<SpeciesName {...requiredProps} />);
+			await user.type(screen.getByLabelText(labelText),"e");
+			expect(screen.getByText(errorSpeciesNameTooShort)).toBeInTheDocument();
+		});
+
+
+		test(`Given use inputs an invalid inputs for Species name (More than 23 characters), 
+		when the component is rendered,
+		The correct error message is displayed`, async() => {
+			const mockChange = jest.fn();
+			const requiredProps: SpeciesNameProps = {
+				speciesName: "morethantwentythreechar",
+				onChange: mockChange,
+			}
+			render(<SpeciesName {...requiredProps} />);
+			await user.type(screen.getByLabelText(labelText),"m");
+			expect(screen.getByText(errorSpeciesNameTooLong)).toBeInTheDocument();
+		});
+
+		test(`Given use inputs an invalid inputs for Species name (number), 
+		when the component is rendered,
+		The correct error message is displayed`, async() => {
+			const mockChange = jest.fn();
+			const requiredProps: SpeciesNameProps = {
+				speciesName: "morethantwentythreec",
+				onChange: mockChange,
+			}
+			render(<SpeciesName {...requiredProps} />);
+			userEvent.clear(screen.getByLabelText(labelText));
+			await user.type(screen.getByLabelText(labelText),"6");
+			expect(screen.getByText(errorSpeciesNameInvalidCharacters)).toBeInTheDocument();
+		});
+
+
+		
 	});
