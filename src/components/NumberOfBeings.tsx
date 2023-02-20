64a7bc885b8a5da436c61458e2896accf89d9e14
JagathA <jagath.ariy@gmail.com>
2023-02-20 19:38:15 +0000

    Changed value type to string

--- src/components/NumberOfBeings.tsx
+++ src/components/NumberOfBeings.tsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
+import ErrorMessage from './ErrorMessage';
 
 export interface NumberOfBeingsProps {
-	numberOfBeings: number;
-	onChange: (newValue:number) => void;
+	numberOfBeings: string;
+	onChange: (newValue: string) => void;
 }
 
-	const NumberOfBeings: React.FC<NumberOfBeingsProps> = ({ numberOfBeings, onChange }) => (
+const errLbl = "ERROR:"
+const NUMBER_OF_BEINGS_MIN = 1000000000;
+
+export const errorNumberOfBeingsTooSmall = `${errLbl} Must be minimum ${NUMBER_OF_BEINGS_MIN}`
+export const errorNumberOfBeingsNotNumber = `${errLbl} Must be a whole number`
+
+const NumberOfBeings: React.FC<NumberOfBeingsProps> = ({ numberOfBeings, onChange }) => {
+
+	const [errorMessage, setErrorMessage] =
+		useState<string | undefined>(undefined);
+
+	const validate: (value: string) => string | undefined = (value) => {
+		const reg = /^[0-9]+$/i;
+
+		if (value.length > 0) {
+			if (!reg.test(value)) {
+				return errorNumberOfBeingsNotNumber;
+			}
+			else if (Number(value) < NUMBER_OF_BEINGS_MIN) {
+				return errorNumberOfBeingsTooSmall;
+			}
+
+		}
+
+		return undefined;
+	}
+
+	return (
 		<>
 			<label htmlFor='NumberOfBeings'>Number of Beings</label>
-			<input id='NumberOfBeings' type='number' value={numberOfBeings} onChange={(e)=>onChange(e.target.valueAsNumber)} />
+			<input id='NumberOfBeings' type='number' value={numberOfBeings} onChange={
+				(e) => {
+					const errorMessage = validate(e.target.value);
+					setErrorMessage(errorMessage);
+					onChange(e.target.value)
+				}}
+			/>
+			<ErrorMessage errorMessage={errorMessage} />
 		</>);
-
-	export default NumberOfBeings;
+}
+export default NumberOfBeings;
