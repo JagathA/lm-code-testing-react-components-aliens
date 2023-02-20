109f8347dbc792d46d7399ba786a5055eebcdd14
JagathA <jagath.ariy@gmail.com>
2023-02-20 13:31:35 +0000

    Added validation  and error message component

--- src/components/PlanetName.tsx
+++ src/components/PlanetName.tsx
@@ -1,14 +1,54 @@
-
+import React from 'react';
+import { useState } from 'react';
+import ErrorMessage from './ErrorMessage';
 
 export interface PlanetNameProps {
 	planetName: string;
 	onChange: (newValue: string) => void;
 }
 
-const PlanetName: React.FC<PlanetNameProps> = ({ planetName, onChange }) => (
+const errLbl = "ERROR:"
+const PLANET_NAME_MIN_LENGTH = 2;
+const PLANET_NAME_MAX_LENGTH = 49;
+
+export const errorPlanetNameTooShort = `${errLbl} Planet Name must be minimum ${PLANET_NAME_MIN_LENGTH} characters`
+export const errorPlanetNameTooLong = `${errLbl} Planet Name must be maximum ${PLANET_NAME_MAX_LENGTH} characters`
+export const errorPlanetNameInvalidCharacters = `${errLbl} No numbers or special characters allowed!`
+
+const PlanetName: React.FC<PlanetNameProps> = ({ planetName, onChange }) => {
+
+	const [errorMessage, setErrorMessage] =
+	useState<string | undefined>(undefined);
+
+const validate: (value: string) => string | undefined = (value) => {
+	const reg = /^[a-z0-9]+$/i;
+	
+	if (value.length > 0) {
+		if (!reg.test(value)) {
+			return errorPlanetNameInvalidCharacters;
+		}
+		else if (value.length < PLANET_NAME_MIN_LENGTH) {
+			return errorPlanetNameTooShort;
+		}
+		else if (value.length > PLANET_NAME_MAX_LENGTH) {
+			return errorPlanetNameTooLong;
+		}
+	}
+
+	return undefined;
+}
+
+	return (
 	<>
 		<label htmlFor='planetName'>Planet Name</label>
-		<input id='planetName' type='text' value={planetName} onChange={(e) => onChange(e.target.value)} />
+		<input id='planetName' type='text' value={planetName} onChange={
+				(e) => {
+					const errorMessage = validate(e.target.value);
+					setErrorMessage(errorMessage);
+					onChange(e.target.value)
+				}}
+			/>
+			<ErrorMessage errorMessage={errorMessage} />
 	</>);
-
+}
 export default PlanetName;
